fd1556291fc09f8dcf4f3d3e6023aee9b871d7cc
tmansy <theo.mansy@gmail.com>
2026-02-15 18:36:26 +0100

    base project

---
+++ project1_reverse/reverse.c
@@ -0,0 +1,22 @@
+/***********************************************
+ * Project: Reverse
+ * 
+ * Group Number :
+ * Students     :
+ * 
+ * Please add details here if the compilation of your project is not
+ * straightforward (for instance, if you use multiple files).
+ ***********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Vous pouvez ajouter d'autres includes si nécessaire
+
+// TODO: ajoutez ici toutes les fonctions auxiliaires que vous souhaitez
+
+int main(int argc, char *argv[]) {
+    //TODO
+    return 0;
+}
